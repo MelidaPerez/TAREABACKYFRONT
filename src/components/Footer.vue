35d1405cb8ce8778e6029a7c3e00b6b377ac5b66
MelidaPerez <cristyordo@gmail.com>
2022-10-23 20:52:10 -0600

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+
+        <div>
+            <footer>
+
+               Melida Perez
+
+             </footer>
+
+        </div>
+
+</template>
+<style  scoped>
+footer {
+    position: fixed;
+    height: 200px;
+    bottom: 0;
+    width: 100%;
+    background-color: blue;
+    color: white;
+}
+</style>
